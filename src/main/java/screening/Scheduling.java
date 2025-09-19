d5089861733f28698877786ac11f74a3cf7d6cb2
hxeyexn <kck85075@kumoh.ac.kr>
2025-09-19 15:33:01 +0900

    refactor(scheduling): 메서드 분리

--- src/main/java/screening/Scheduling.java
+++ src/main/java/screening/Scheduling.java
@@ -3,6 +3,7 @@ package screening;
 import movie.Movie;
 import theater.Theater;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,19 +16,55 @@ public class Scheduling {
         List<Theater> theaters
     ) {
         for (Theater theater : theaters) {
-            LocalDateTime currentTime = theater.getOpenTime();
-            int index = 0;
-
-            while(true) {
-                Movie movie = movies.get(index);
-                LocalDateTime end = currentTime.plusMinutes(movie.getScreeningTime());
-                if (end.toLocalTime().isAfter(theater.getCloseTime().toLocalTime())) break;
-
-                addScreening(theater, movie, currentTime, end);
-                currentTime = end.plusMinutes(CLEANING_TIME);
-                index = (index + 1) % movies.size();
-            }
+            scheduleScreeningFor(theater, movies);
         }
+    }
+
+    private void scheduleScreeningFor(
+        Theater theater,
+        List<Movie> movies
+    ) {
+        LocalDateTime currentTime = theater.getOpenTime();
+        int index = 0;
+
+        while(true) {
+            Movie movie = movies.get(index);
+            LocalDateTime endTime = calculateNextEndTime(currentTime, movie);
+            if (isTheaterClosed(endTime, theater)) break;
+
+            addScreening(theater, movie, currentTime, endTime);
+            currentTime = calculateNextStartTime(endTime);
+            index = calculateNextIndex(index, movies);
+        }
+    }
+
+    private LocalDateTime calculateNextEndTime(
+        LocalDateTime currentTime,
+        Movie movie
+    ) {
+        int screeningTime = movie.getScreeningTime();
+        return currentTime.plusMinutes(screeningTime);
+    }
+
+    private boolean isTheaterClosed(
+        LocalDateTime endTime,
+        Theater theater
+    ) {
+        LocalTime screeningEndTime = endTime.toLocalTime();
+        LocalTime operatingEndTime = theater.getCloseTime().toLocalTime();
+
+        return screeningEndTime.isAfter(operatingEndTime);
+    }
+
+    private LocalDateTime calculateNextStartTime(LocalDateTime endTime) {
+        return endTime.plusMinutes(CLEANING_TIME);
+    }
+
+    private int calculateNextIndex(
+        int index,
+        List<Movie> movies
+    ) {
+        return (index + 1) % movies.size();
     }
 
     private void addScreening(
